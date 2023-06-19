33be16fc5b4a8c4b3078b4dba9ab971b0c07feb0
MakG-Go <92160024+MakG-Go@users.noreply.github.com>
2023-06-19 13:22:48 +0300

    correct
    Small_correct

--- src/js/modules/createTemplate.js
+++ src/js/modules/createTemplate.js
@@ -6,16 +6,16 @@ export function generateUser(user, id) {
     <div class="col-lg-12 col-xs-6 js-serch-item js-no-serch">
         <div class="row user-list__item">
             <div class="col-lg-3 col-xs-6">
-                <span class="text-m text-bold text-blue js-username">${user.username}</span>
+            <span class="text-m hide-lg">Имя пользователя: </span> <span class="text-m text-bold text-blue js-username">${user.username}</span>
             </div>
             <div class="col-lg-3 col-xs-6">
-                <span class="text-m text-grey-1 js-email">${user.email}</span>
+            <span class="text-m hide-lg">E-mail: </span><span class="text-m text-grey-1 js-email">${user.email}</span>
             </div>
             <div class="col-lg-3 col-xs-6">
-                <span class="text-m text-grey-1">${user.registration_date}</span>
+            <span class="text-m hide-lg">Дата регистрации: </span><span class="text-m text-grey-1">${user.registration_date}</span>
             </div>
             <div class="col-lg-3 col-xs-6 flex">
-                <span class="text-m text-grey-1">${user.rating}</span>
+            <span class="text-m hide-lg">Рейтинг: </span><span class="text-m text-grey-1">${user.rating}</span>
                 <button class="btn btn-delite js-removeUser" id="${id}">&#x2716;</button>
             </div>
         </div>
